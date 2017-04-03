883c1e0a9cde49071308f9b4c3821619cfc47925
Peter Grønbæk Andersen <peter.groenbaek@gmail.com>
2017-04-03 13:52:31 +0200

    It works again

--- Arduino/SmartSleeper/SmartSleeper.ino
+++ Arduino/SmartSleeper/SmartSleeper.ino
@@ -10,10 +10,10 @@
 #define DEBUG 1
 
 struct data {
-  bool tempBits[15];
-  bool luxBits[20];
-  bool humBits[14];
-  bool psmBits[11];
+  boolean tempBits[15];
+  boolean luxBits[20];
+  boolean humBits[14];
+  boolean psmBits[11];
 };
 
 void setup() {
@@ -22,54 +22,55 @@ void setup() {
   smePressure.begin();
   smeAmbient.begin();
   smeHumidity.begin();
-  SerialUSB.begin(115200);
+  SerialUSB.begin(19200);
 
   SigFox.begin(19200);
-  
+
   //initSigfox();
 }
 
 void loop() {
   SerialUSB.println(millis());
-  
+
   int temperature = transformTemperature(readTemp());
   int lux =  transformLux(readLux());
   int hum = transformHumidity(readHum());
   int psm = transformPressure(readBarometer());
-  
+
   data frame;
-  
-  bitify(temperature,15,frame.tempBits);
-  bitify(lux,20,frame.luxBits);
-  bitify(hum,14,frame.humBits);
-  bitify(psm,11,frame.psmBits);
+
+  bitify(temperature, 15, frame.tempBits);
+  bitify(lux, 20, frame.luxBits);
+  bitify(hum, 14, frame.humBits);
+  bitify(psm, 11, frame.psmBits);
 
   SerialUSB.println(getSigfoxFrame(&frame, sizeof(data)));
+  SerialUSB.println(sizeof(data));
   //bool answer = sendSigfox(&frame, sizeof(data));
-  
-  SerialUSB.println(temperature + "\n");
-  SerialUSB.println(lux + "\n");
-  SerialUSB.println(hum + "\n");
-  SerialUSB.println(psm + "\n");
-  
+
+  //SerialUSB.println(temperature);
+  //SerialUSB.println(lux);
+  //SerialUSB.println(hum);
+  //SerialUSB.println(psm);
+
   delay(INTERVAL);
 }
 
 /**
- * Read the temperature sensor, returns a float
- */
+   Read the temperature sensor, returns a float
+*/
 float readTemp() {
   return smeHumidity.readTemperature();
 }
 
 /**
- * Transform temperature measurements:
- * Input datarange: -163.84 - 163.83
- * Output datarange: 0 - 32766
- * Size: 15 bits
- * 
- * TODO: Put some hardcoded values in '#define's
- */
+   Transform temperature measurements:
+   Input datarange: -163.84 - 163.83
+   Output datarange: 0 - 32766
+   Size: 15 bits
+
+   TODO: Put some hardcoded values in '#define's
+*/
 int transformTemperature(float temperature) {
   temperature += 163.84f;
   temperature *= 100;
@@ -79,22 +80,22 @@ int transformTemperature(float temperature) {
 }
 
 /**
- * Read the humidity sensor, returns a float
- * Datarange: 0.00 - 163.83
- * Size: 14 bits
- */
+   Read the humidity sensor, returns a float
+   Datarange: 0.00 - 163.83
+   Size: 14 bits
+*/
 float readHum() {
   return smeHumidity.readHumidity();
 }
 
 /**
- * Transform humidity measurements:
- * Input datarange: 0.00 - 163.83
- * Output datarange: 0 - 16383
- * Size: 14 bits
- * 
- * TODO: Put some hardcoded values in '#define's
- */
+   Transform humidity measurements:
+   Input datarange: 0.00 - 163.83
+   Output datarange: 0 - 16383
+   Size: 14 bits
+
+   TODO: Put some hardcoded values in '#define's
+*/
 int transformHumidity(float humidity) {
   humidity *= 100;
   int transformedHumidity = (int) humidity;
@@ -103,10 +104,10 @@ int transformHumidity(float humidity) {
 }
 
 /**
- * Read the Barometer sensor returns a int
- * This sensor have 11 bits to take in the datastrame.
- * Datarange of 0-2047
- */
+   Read the Barometer sensor returns a int
+   This sensor have 11 bits to take in the datastrame.
+   Datarange of 0-2047
+*/
 float readBarometer() {
   return smePressure.readPressure();
 }
@@ -118,22 +119,22 @@ int transformPressure(float pressure) {
 }
 
 /**
- * Read the Lux sensor returns a float, in other to get the lux value you need to / 10000
- * This sensor have 20 bits to take in the datastrame.
- * Datarange of 0.00-10465.27
- */
+   Read the Lux sensor returns a float, in other to get the lux value you need to / 10000
+   This sensor have 20 bits to take in the datastrame.
+   Datarange of 0.00-10465.27
+*/
 float readLux() {
   return smeAmbient.ligthPollingRead() / 10000.0;
 }
 
 /**
- * Transform humidity measurements:
- * Input datarange: 0.00 - 10465.27
- * Output datarange: 0 - 1046527
- * Size: 14 bits
- * 
- * TODO: Put some hardcoded values in '#define's
- */
+   Transform humidity measurements:
+   Input datarange: 0.00 - 10465.27
+   Output datarange: 0 - 1046527
+   Size: 14 bits
+
+   TODO: Put some hardcoded values in '#define's
+*/
 int transformLux(float lux) {
   lux *= 100;
   int transformedLux = (int) lux;
@@ -142,45 +143,46 @@ int transformLux(float lux) {
 }
 
 /**
- *  prints val with number of decimal places determine by precision
- *  NOTE: precision is 1 followed by the number of zeros for the desired number of decimal places
- *  example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
- */
-void printDouble(double val, unsigned int precision){
-    SerialUSB.print (int(val));  //prints the int part
-    SerialUSB.print("."); // print the decimal point
-    unsigned int frac;
-    if(val >= 0)
-        frac = (val - int(val)) * precision;
-    else
-        frac = (int(val)- val ) * precision;
-    int frac1 = frac;
+    prints val with number of decimal places determine by precision
+    NOTE: precision is 1 followed by the number of zeros for the desired number of decimal places
+    example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
+*/
+void printDouble(double val, unsigned int precision) {
+  //SerialUSB.print (int(val));  //prints the int part
+  //SerialUSB.print("."); // print the decimal point
+  unsigned int frac;
+  if (val >= 0)
+    frac = (val - int(val)) * precision;
+  else
+    frac = (int(val) - val ) * precision;
+  int frac1 = frac;
 
-    while( frac1 /= 10 )
-        precision /= 10;
+  while ( frac1 /= 10 )
     precision /= 10;
+  precision /= 10;
 
-    while(  precision /= 10)
-        SerialUSB.print("0");
+  while (  precision /= 10) {
+    //SerialUSB.print("0");
+  }
 
-    SerialUSB.println(frac,DEC) ;
+  //SerialUSB.println(frac, DEC) ;
 }
 
 /**
- * used to bitify an int.
- * input is the int to bitify
- * the amont of bits to bitify
- * averagingBuffer boolean array to save the result in 
- */
-void bitify(unsigned int input,unsigned int bitSize, boolean *averagingBuffer){
-  SerialUSB.print(bitSize);
-  for (int i=bitSize-1; i >= 0; i--){
-    if(input >= pow(2,i))
+   used to bitify an int.
+   input is the int to bitify
+   the amont of bits to bitify
+   averagingBuffer boolean array to save the result in
+*/
+void bitify(unsigned int input, unsigned int bitSize, boolean *averagingBuffer) {
+  //SerialUSB.print(bitSize);
+  for (int i = bitSize - 1; i >= 0; i--) {
+    if (input >= pow(2, i))
     {
       averagingBuffer[i] = true;
-      input -= pow(2,i);
+      input -= pow(2, i);
     }
-    else{
+    else {
       averagingBuffer[i] = false;
     }
   }
@@ -188,18 +190,18 @@ void bitify(unsigned int input,unsigned int bitSize, boolean *averagingBuffer){
 
 
 /**
- * Ensure that data is within a certain range.
- * Transform the data to an int before using this if you have a float.
- * 
- * min: lower bound
- * max: higher bound
- * value: surprise, this is the value to check! :D
- */
+   Ensure that data is within a certain range.
+   Transform the data to an int before using this if you have a float.
+
+   min: lower bound
+   max: higher bound
+   value: surprise, this is the value to check! :D
+*/
 int ensureDatarange(int min, int max, int value) {
-  if(value < min) {
+  if (value < min) {
     value = min;
   }
-  else if(value > max) {
+  else if (value > max) {
     value = max;
   }
   return value;
@@ -214,7 +216,7 @@ void initSigfox() {
   }
   while (SigFox.available()) {
     byte serialByte = SigFox.read();
-    if (DEBUG){
+    if (DEBUG) {
       SerialUSB.print(serialByte);
     }
   }
@@ -223,32 +225,38 @@ void initSigfox() {
   }
 }
 
-String getSigfoxFrame(const void* data, uint8_t len){
+String getSigfoxFrame(const void* data, uint8_t len) {
   String frame = "";
-  uint8_t* bytes = (uint8_t*)data;
-  
-  if (len < SIGFOX_FRAME_LENGTH){
+  byte* bytes = (byte*)data;
+  SerialUSB.println("bytes:");
+  SerialUSB.println(sizeof(bytes));
+  SerialUSB.println("len:");
+  SerialUSB.println(len);
+
+  /*if (len < SIGFOX_FRAME_LENGTH) {
     //fill with zeros
     uint8_t i = SIGFOX_FRAME_LENGTH;
-    while (i-- > len){
+    while (i-- > len) {
       frame += "00";
     }
-  }
+  }*/
 
   //0-1 == 255 --> (0-1) > len
-  for(uint8_t i = len-1; i < len; --i) {
-    if (bytes[i] < 16) {frame+="0";}
+  for (uint8_t i = len - 1; i < len; --i) {
+    if (bytes[i] < 16) {
+      frame += "0";
+    }
     frame += String(bytes[i], HEX);
   }
-  
+
   return frame;
 }
 
-bool sendSigfox(const void* data, uint8_t len){
+bool sendSigfox(const void* data, uint8_t len) {
   String frame = getSigfoxFrame(data, len);
   String status = "";
   char output;
-  if (DEBUG){
+  if (DEBUG) {
     SerialUSB.print("AT$SF=");
     SerialUSB.println(frame);
   }
@@ -256,21 +264,21 @@ bool sendSigfox(const void* data, uint8_t len){
   SigFox.print(frame);
   SigFox.print("\r");
   while (!SigFox.available());
-  
-  while(SigFox.available()){
+
+  while (SigFox.available()) {
     output = (char)SigFox.read();
     status += output;
     delay(10);
   }
-  if (DEBUG){
+  if (DEBUG) {
     SerialUSB.print("Status \t");
     SerialUSB.println(status);
   }
-  if (status == "OK\r"){
+  if (status == "OK\r") {
     //Success :)
     return true;
   }
-  else{
+  else {
     return false;
   }
 }
